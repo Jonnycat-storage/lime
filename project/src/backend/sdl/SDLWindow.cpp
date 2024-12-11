0819590e4e47728971ca59850b243463ebefa948
jonnythecoder <107599365+JonnycatMeow@users.noreply.github.com>
2024-12-11 14:38:25 -0800

    oops fixed the definition

--- project/src/backend/sdl/SDLWindow.cpp
+++ project/src/backend/sdl/SDLWindow.cpp
@@ -107,7 +107,7 @@ namespace lime {
 			#endif
 			
                         //Testing glcore for macos 
-			#if defined (MACOS)
+			#if defined (HX_MACOS)
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
